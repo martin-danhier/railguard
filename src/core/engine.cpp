cbd4268fcbb952e71fa6b159b739a6ef8125db08
Martin Danhier <martin.danhier@student.uclouvain.be>
2022-03-24 13:51:10 +0100

    Initial commit

---
+++ src/core/engine.cpp
@@ -0,0 +1,1 @@
+#include "railguard/core/engine.h"
---
+++ tests/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int arc, char** argv)
+{
+    std::cout << "Hello, World!" << std::endl;
+    
+
+    return 0;
+}
