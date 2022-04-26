5415928fc3d086d8928275ba8f0d37a19d0accc6
Martin Danhier <martin.danhier@student.uclouvain.be>
2022-04-26 21:37:00 +0200

    test ci

---
+++ tests/core/window_api.cpp
@@ -0,0 +1,23 @@
+#include <test_framework/test_framework.hpp>
+
+#ifdef WINDOW_SDL2
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+TEST
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_WindowFlags window_flags;
+
+// We need to know which renderer to use
+#ifdef RENDERER_VULKAN
+    window_flags = SDL_WINDOW_VULKAN;
+#endif
+
+    SDL_Window *window = SDL_CreateWindow("Test window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, window_flags);
+    ASSERT_NOT_NULL(window);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+#endif
